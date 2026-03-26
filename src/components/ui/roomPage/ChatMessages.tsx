dcd9e8091d6e47ed93a21b343bd9c4994c9e7e33
Novice2311 <igordobrica60@gmail.com>
2026-03-26 18:14:33 +0300

    ui room page template

---
+++ src/components/ui/roomPage/ChatMessages.tsx
@@ -0,0 +1,9 @@
+const ChatMessages = () => {
+  return (
+    <div className="flex-1 overflow-y-auto p-4">
+      <p className="text-gray-400 text-sm">Сообщения чата будут здесь</p>
+    </div>
+  );
+};
+
+export default ChatMessages;
---
+++ src/components/ui/roomPage/Participants.tsx
@@ -0,0 +1,9 @@
+const Participants = () => {
+  return (
+    <div className="p-4 border-b">
+      <h2 className="font-semibold">Участники (3)</h2>
+    </div>
+  );
+};
+
+export default Participants;
